dd354bcbd7af3d8a0f775ab8737ee82127d4bda9
Eugstea1 <andrin@eugsters.ch>
2023-04-01 09:34:30 +0200

    implement client app

--- source/api/smartSpaceAPI.py
+++ source/api/smartSpaceAPI.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
